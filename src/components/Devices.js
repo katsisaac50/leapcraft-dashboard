7159256aaa25fe8123974004f5a64de91425d6c4
katongole <katsisaac50@gmail.com>
2018-10-19 15:17:35 +0200

    maped data to render a list of devices.

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 class Devices extends Component {
+    constructor(){
+        super()
+        this.state={
+            devices:{}
+        }
+    }
     render (){
-        console.log(this);
-        return <div>Devices</div>;
+        const {data} =this.props.devices
+        console.log(data);
+        /* sessionStorage.setItem('key', JSON.stringify(data))
+        var data1 = sessionStorage.getItem('key'); */
+        
+        let listDevices = data.map(devices => <li key={devices.box_id}>{devices.box_label}</li>)
+        return <div>Devices
+       <ul>{listDevices}</ul>
+        </div>;
     }
 }
 
