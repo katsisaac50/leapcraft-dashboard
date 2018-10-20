379f65f852a0a578fcddd52df6642bd81fe1e0fe
katongole <katsisaac50@gmail.com>
2018-10-20 11:54:23 +0200

    admin modified to signin

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Redirect, Link, Route, Switch } from "react-router-dom";
 import Login, {appAuth} from "./Login";
+import Header from './Header';
 
 class App extends Component {
   constructor(props){
@@ -14,23 +15,20 @@ class App extends Component {
     this.setState({ devices: devices });
 }
 
+showLock(){
+  window.location.href=`/signin`;
+}
+
   render() {
     return (
       <BrowserRouter>
       <div>
-        <nav className="navbar navbar">
-          <ul className="nav">
-            <li>
-              <Link to="/admin">Admin area</Link>
-            </li>
-          </ul>
-        </nav>
-
+      <Header onLoginClick={this.showLock.bind(this)} />
         <Switch>
         <Route path='/login'
-        render = {(props) => <Login {...props} setDevices={this.setDevices.bind(this)} />}
-    ></Route>
-          <PrivateRoute path="/admin" component={Admin} devices={this.state.devices}/>
+        render = {(props) => <Login {...props} setDevices={this.setDevices.bind(this)} />}>
+        </Route>
+        <PrivateRoute path="/signin" component={Admin} devices={this.state.devices}/>
         </Switch>
       </div>
       </BrowserRouter>
@@ -69,7 +67,7 @@ return null;
         let listDevices = data.map(devices => <li key={devices.box_id}>{devices.box_label}</li>)
   return (
     
-    <div>Devices
+    <div /* id='logo-container' */>Devices
        <ul>
        {listDevices}
        </ul>
